9c669bbeb595f1bb82a04111e1cfbe8bdbb1f7c8
Muhammad Yusuf Habibie <myh481813@gmail.com>
2024-10-03 09:49:48 +0700

    add gasReporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("hardhat-gas-reporter");
 require("dotenv").config();
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
@@ -22,4 +23,12 @@ module.exports = {
     },
   },
   solidity: "0.8.27",
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    L1Etherscan: ETHERSCAN_API_KEY,
+  },
 };
