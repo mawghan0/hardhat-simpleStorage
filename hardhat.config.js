97b36a3e4d7630454893ec30679e7166d18ad4ce
Muhammad Yusuf Habibie <myh481813@gmail.com>
2024-10-03 09:56:22 +0700

    add solidityCoverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-gas-reporter");
 require("dotenv").config();
+require("solidity-coverage");
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
